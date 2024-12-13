0262eea7f8ca15954e94e16925f444244f4a2943
Wendril Gabriel <90289090+WendrilXX@users.noreply.github.com>
2024-12-13 00:11:52 -0300

    Delete CETI/src directory

--- CETI/src/js/navbar.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('navbarToggle').addEventListenerdocument.getElementById('navbarToggle').addEventListener('click', function () {
-    document.getElementById('navbarLinks').classList.toggle('active');
-});
