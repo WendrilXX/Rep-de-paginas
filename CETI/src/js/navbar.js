20c1d869904fa3acf9375465563c509f56f87397
Wendril Gabriel <90289090+WendrilXX@users.noreply.github.com>
2024-12-13 00:12:33 -0300

    CETI-PI
    Pagina simples para uma escola

---
+++ CETI/src/js/navbar.js
@@ -0,0 +1,4 @@
+document.getElementById('navbarToggle').addEventListener('click', function () {
+    const navbarLinks = document.getElementById('navbarLinks');
+    navbarLinks.classList.toggle('active');
+});
